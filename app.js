c1e4de276c7dc1cc8693accf0aa75da2c70f2e59
unknown <201951073@iiitvadodara.ac.in>
2021-04-24 16:50:19 +0530

    errr

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const path = require('path');
-
+const dotenv = require("dotenv")
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const db = require('./util/database');
 //const Users = require('./models/user')
-
+dotenv.config();
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -45,6 +45,6 @@ app.use(errorController.get404);
 // })
 
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 console.log("App is Up!!")
