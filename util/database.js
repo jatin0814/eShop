44d2ccb9f9915b13ce697e4a3d7ff4e958f7b260
unknown <201951073@iiitvadodara.ac.in>
2021-04-28 13:25:02 +0530

    init

--- util/database.js
+++ util/database.js
@@ -1,17 +1,17 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'sql6.freemysqlhosting.net',
-    user: 'sql6407949',
-    database: 'sql6407949',
-    password: 'XGxuzmhtbJ'
+    host: 'localhost',
+    user: 'root',
+    database: 'e_shop',
+    password: 'toor'
 });
 
 pool.execute("CREATE TABLE `users` (`userId` INT NOT NULL AUTO_INCREMENT,`userName` VARCHAR(100) NOT NULL,`password` VARCHAR(45) NOT NULL,`email` VARCHAR(45) NOT NULL,PRIMARY KEY (`userId`));",(err,res)=>{
     
 })
 
-pool.execute("CREATE TABLE `products` (`productId` INT NOT NULL AUTO_INCREMENT,`userId` INT NOT NULL,`title` VARCHAR(100) NOT NULL,`price` INT NOT NULL,`image` VARCHAR(255) NOT NULL,`description` VARCHAR(255) NOT NULL,PRIMARY KEY (`productId`));",(err,res)=>{
+pool.execute("CREATE TABLE `products` (`productId` INT NOT NULL AUTO_INCREMENT,`userId` INT NOT NULL,`title` VARCHAR(100) NOT NULL,`price` INT NOT NULL,`image` VARCHAR(255) NOT NULL,`description` VARCHAR(2255) NOT NULL,PRIMARY KEY (`productId`));",(err,res)=>{
     
 })
 
